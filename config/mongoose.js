1e08625ee381ffe09da858e3b55718f5e2c5fe50
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-09-28 15:31:22 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(process.env.DATABASE_URL);
-//mongoose.connect(process.env.mongooseUrl);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error Connecting to Database!'));
